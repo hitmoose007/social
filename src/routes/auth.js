9a86d2c9f53c53c8dcaeb87d9ce73f2947b50e84
Moosa Butt <moosabutt715@gmail.com>
2022-01-31 19:58:53 +0500

    cleaned up stuff

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,31 +4,19 @@ const { comparePassword, hashPassword } = require("../utils/hash");
 const jwt = require("jsonwebtoken");
 
 router.post("/login", login);
-router.post('/register', register);
 // router.post('/logout', logout);
 // router.post('/forgot-password', forgotPassword);
 // router.post('/reset-password', resetPassword);
 
-async function register(req, res){
-    const prisma = new PrismaClient();
-    const { email, password } = req.body;
-    const hashedPassword = await hashPassword(password);
-    const user = await prisma.user.create({
-        data: {
-        email : email,
-        password: hashedPassword,
-        },
-    });
-    res.json({
-        user,
-    });
+
 }
 
 async function login(req, res) {
   jwt.sign({ foo: "bar" }, "privateKey", function (err, token) {
-    res.json({
-      token,
-    });
+      //print out error if there is one
+        if (err) {
+            console.log(err);
+        }      
   });
 }
 
