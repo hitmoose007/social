7b38cb53301d90c7d8cf3a239a35dbd729fc1baa
HADO564 <70658014+HADO564@users.noreply.github.com>
2022-04-12 01:44:03 +0500

    Deleted unused index file

--- src/routes/friend/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router({mergeParams: true});
-//mess with this file. idk why i made it, but im thinking about the routes and i dont think this should exist
-router.use('/',() => {});
-
-
-module.exports = router;
